fbead777a079868b70f426051161295e2c921e88
koenmetsu <koen@kometsoft.be>
2018-11-16 17:05:30 +0100

    formatted

--- test/rover.test.js
+++ test/rover.test.js
@@ -1,5 +1,8 @@
 class Rover {
-    constructor(position = {x: 0, y:0}){
+    constructor(position = {
+        x: 0,
+        y: 0
+    }) {
 
         this._position = position;
     }
@@ -24,34 +27,49 @@ class Rover {
     }
 
     backward() {
-        if(this._position.y === 0) {
+        if (this._position.y === 0) {
             this._position.y = 10
         } else {
-            this._position.y -= 1 
+            this._position.y -= 1
         }
     }
 }
 
 describe("rover starting position", () => {
     it("should have 0, 0 as starting position", () => {
-        expect(new Rover().position()).toEqual({x: 0, y: 0});
+        expect(new Rover().position()).toEqual({
+            x: 0,
+            y: 0
+        });
     });
 
     it("should move to 0, 1 when command f is received", () => {
         const rover = new Rover();
         rover.execute("f");
-        expect(rover.position()).toEqual({x: 0, y: 1});
+        expect(rover.position()).toEqual({
+            x: 0,
+            y: 1
+        });
     });
 
     it("should move to 0, 0 when command b is received on position 0,1", () => {
-        const rover = new Rover({x: 0, y: 1});
+        const rover = new Rover({
+            x: 0,
+            y: 1
+        });
         rover.execute("b");
-        expect(rover.position()).toEqual({x: 0, y: 0});
+        expect(rover.position()).toEqual({
+            x: 0,
+            y: 0
+        });
     });
 
     it("should move to 0, 10 when command b is received on position 0,0", () => {
         const rover = new Rover();
         rover.execute("b");
-        expect(rover.position()).toEqual({x: 0, y: 10});
+        expect(rover.position()).toEqual({
+            x: 0,
+            y: 10
+        });
     });
 });
